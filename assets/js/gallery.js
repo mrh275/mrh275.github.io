e4a5c9ab936d517731be808d065bd7a53b82e59a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-04-18 11:37:23 +0700

    create event click function to call lightgallery

---
+++ assets/js/gallery.js
@@ -0,0 +1,148 @@
+// Verval Ponsel
+const vervalPonsel = document.getElementById("vervalPonsel");
+const vervalGallery = window.lightGallery(vervalPonsel, {
+  dynamic: true,
+  plugins: [lgZoom, lgThumbnail],
+  dynamicEl: [
+    {
+      src: "http://localhost:3000/assets/img/project/verval-ponsel.png",
+      thumb: "http://localhost:3000/assets/img/project/verval-ponsel.png",
+      subHtml: '<h4>Portal - Verval Ponsel</h4><a href="https://vervalponsel.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/verval-ponsel/verval-ponsel-1.png",
+      thumb: "http://localhost:3000/assets/img/project/verval-ponsel/verval-ponsel-1.png",
+      subHtml: '<h4>Login - Verval Ponsel</h4><a href="https://vervalponsel.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/verval-ponsel/verval-ponsel-2.png",
+      thumb: "http://localhost:3000/assets/img/project/verval-ponsel/verval-ponsel-2.png",
+      subHtml: '<h4>Admin Panel - Verval Ponsel</h4><a href="https://vervalponsel.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/verval-ponsel/verval-ponsel-3.png",
+      thumb: "http://localhost:3000/assets/img/project/verval-ponsel/verval-ponsel-3.png",
+      subHtml: '<h4>Admin Edit Siswa - Verval Ponsel</h4><a href="https://vervalponsel.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/verval-ponsel/verval-ponsel-4.png",
+      thumb: "http://localhost:3000/assets/img/project/verval-ponsel/verval-ponsel-4.png",
+      subHtml: '<h4>Edit Profil - Verval Ponsel</h4><a href="https://vervalponsel.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/verval-ponsel/verval-ponsel-5.png",
+      thumb: "http://localhost:3000/assets/img/project/verval-ponsel/verval-ponsel-5.png",
+      subHtml: '<h4>Edit Nomor Ponsel - Verval Ponsel</h4><a href="https://vervalponsel.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/verval-ponsel/verval-ponsel-6.png",
+      thumb: "http://localhost:3000/assets/img/project/verval-ponsel/verval-ponsel-6.png",
+      subHtml: '<h4>Ubah Kata Sandi - Verval Ponsel</h4><a href="https://vervalponsel.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+  ],
+});
+
+// Semesta
+const semesta = document.getElementById("semesta");
+const semestaGallery = window.lightGallery(semesta, {
+  dynamic: true,
+  plugins: [lgZoom, lgThumbnail],
+  dynamicEl: [
+    {
+      src: "http://localhost:3000/assets/img/project/semesta/semesta-1.png",
+      thumb: "http://localhost:3000/assets/img/project/semesta/semesta-1.png",
+      subHtml: '<h4>Home - Semesta</h4><a href="https://sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/semesta/semesta-dark.png",
+      thumb: "http://localhost:3000/assets/img/project/semesta/semesta-dark.png",
+      subHtml: '<h4>Home (Dark Mode) - Semesta</h4><a href="https://sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/semesta/semesta-2.png",
+      thumb: "http://localhost:3000/assets/img/project/semesta/semesta-2.png",
+      subHtml: '<h4>Article Page - Semesta</h4><a href="https://sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/semesta/semesta-3.png",
+      thumb: "http://localhost:3000/assets/img/project/semesta/semesta-3.png",
+      subHtml: '<h4>Contact Form - Semesta</h4><a href="https://sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+  ],
+});
+
+// Jejak Lulusan
+const jejakLulusan = document.getElementById("jejakLulusan");
+const lulusanGallery = window.lightGallery(jejakLulusan, {
+  dynamic: true,
+  plugins: [lgZoom, lgThumbnail],
+  dynamicEl: [
+    {
+      src: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-1.png",
+      thumb: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-1.png",
+      subHtml: '<h4>Home - Jejak Lulusan</h4><a href="https://alumni.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-2.png",
+      thumb: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-2.png",
+      subHtml: '<h4>Admin Dashboard - Jejak Lulusan</h4><a href="https://alumni.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-3.png",
+      thumb: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-3.png",
+      subHtml: '<h4>Admin Tabel Biodata Lulusan - Jejak Lulusan</h4><a href="https://alumni.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-4.png",
+      thumb: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-4.png",
+      subHtml: '<h4>Admin Edit Biodata Lulusan - Jejak Lulusan</h4><a href="https://alumni.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-5.png",
+      thumb: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-5.png",
+      subHtml: '<h4>Admin Tabel Data Orang Tua Lulusan - Jejak Lulusan</h4><a href="https://alumni.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-6.png",
+      thumb: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-6.png",
+      subHtml: '<h4>Admin Edit Data Orang Tua Lulusan - Jejak Lulusan</h4><a href="https://alumni.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-7.png",
+      thumb: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-7.png",
+      subHtml: '<h4>Admin Tabel Kuliah Lulusan - Jejak Lulusan</h4><a href="https://alumni.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-8.png",
+      thumb: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-8.png",
+      subHtml: '<h4>Dashboard Siswa - Jejak Lulusan</h4><a href="https://alumni.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-9.png",
+      thumb: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-9.png",
+      subHtml: '<h4>Form Edit Biodata Siswa - Jejak Lulusan</h4><a href="https://alumni.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-10.png",
+      thumb: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-10.png",
+      subHtml: '<h4>Form Edit Data Orang Tua Siswa - Jejak Lulusan</h4><a href="https://alumni.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+    {
+      src: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-11.png",
+      thumb: "http://localhost:3000/assets/img/project/jejak-lulusan/jejak-lulusan-11.png",
+      subHtml: '<h4>Form Edit Data Kuliah Siswa - Jejak Lulusan</h4><a href="https://alumni.sman1rawamerta.sch.id" target="_blank" style="font-size:.9rem;">Visit Project</a>',
+    },
+  ],
+});
+
+vervalPonsel.addEventListener("click", () => {
+  vervalGallery.openGallery();
+});
+
+semesta.addEventListener("click", () => {
+  semestaGallery.openGallery();
+});
+
+jejakLulusan.addEventListener("click", () => {
+  lulusanGallery.openGallery();
+});
